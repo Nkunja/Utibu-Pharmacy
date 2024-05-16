092b3d53c9d1af4aad7e4d0341c596e4a881fe46
Nkunja <munene01denis@gmail.com>
2024-05-16 16:24:34 +0300

    env gitignore

--- pharmacy/settings.py
+++ pharmacy/settings.py
@@ -39,8 +39,9 @@ DEBUG = os.getenv('DEBUG')
 
 ALLOWED_HOSTS = ['*']
 
-CSRF_TRUSTED_ORIGINS = ['https://gnu-cosmic-civet.ngrok-free.app',
-                        'https://utibu-pharmacy.onrender.com',
+
+
+CSRF_TRUSTED_ORIGINS = ['https://utibu-pharmacy.onrender.com',
                         'http://127.0.0.1:8000',
                         'exp://192.168.1.234:19000']
 
